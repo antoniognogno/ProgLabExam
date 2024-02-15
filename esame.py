7622c64a267a110f3c5d3ff98ba0eadb490a3c0b
antoniognogno <78216046+antoniognogno@users.noreply.github.com>
2024-02-15 18:39:15 +0000

    Last

--- esame.py
+++ esame.py
@@ -1,10 +1,27 @@
 #Per ovviare al problema delle date in ordine crescente, ho pensato di tenere traccia di un max tra le date, e se andando avanti questo valore NON viene superato, vuol dire che c'è una data fuori posto, e quindi alzo un eccezione.
 
+#******************************
+#  Gestione casi speciali
+#******************************
+
+#CASO 1
+#Se l'anno inziale e l'anno finale coincidono, returno semplicemente l'anno desiderato col suo valore di media. Tuttavia se l'anno inserito è lo stesso ma non presenta alcuna misurazione, viene returnata una lista vuota
+
+#CASO 2
+#se l'anno iniziale e finale sono due anni che non presentano dati, ma sono presenti nel file, e hanno in mezzo un anno con dati presenti. ALlora stampo l'anno in mezzo. Esempio 1950 e 1952. Dove 1950 e 1952 non hanno dati, ma 1951 si, stampa quello (1951)
+
+#CASO 3
+#se last year passato come parameteo non ha dati al suo interno, interrompe il ciclo. Casistica intervallo '49-'52, dati mancanti 1950 e 1952. Ideologicamente dovrebbe restituire 1949-1950 1950-1951 1951-1952. Ma dato che mancano quei due anni deve restituire 1949-1951 e basta. Questo controllo impedisce di memorizzare anche 1951-1953 che andrebbe oltre il limite alto imposto
+
 
 #==============================
 #  Libreria per regex, utilizzata in get_data() per verificare il pattern della data "YYYY-MM"
 #==============================
 import re
+
+
+#inizializzo una lista vuota che conterrà tutti gli anni privi di almeno una misurazione. globale
+anni_vuoti = []
 
 #==============================
 #  Classe ExamException da usare per istanziare eccezioni
@@ -56,24 +73,33 @@ class CSVTimeSeriesFile:
                 if verifica_pattern(elements[0]):
                     try:
                         #Conversione valore in intero
-                        elements[1] = int(elements[1])
+                            elements[1] = int(elements[1])
+
+                            #se il valore è negativo, lo saltiamo
+                            if elements[1] < 0:
+                                continue
+                                
                     except Exception as e:
-                        print('Errore in conversione del valore "{}" a numerico: "{}"'.format(elements[1], e))
+                        # print('Errore in conversione del valore "{}" a numerico: "{}"'.format(elements[1], e))
+
+                        #se è vuoto aggiungo ad una lista di appoggio che servirà dopo per verificare che i parametri di compute_increments erano presenti nel file iniziale, anche se senza dati
+                        if elements[1] == "":
+                            anno_vuoto_pulito = elements[0].split('-')[0]
+                            if anno_vuoto_pulito not in anni_vuoti:
+                                anni_vuoti.append(anno_vuoto_pulito)
+
+                        
                         #continue utilizzato per saltare l'iterazione in cui non è possibile avere tipo di dato int, senza bloccare il codice
                         continue
                 else: 
-                    print('Errore, pattern data errato')
+                    # print('Errore, pattern data errato')
                     continue
 
-                # print(elements[0])
                 anno_mese = elements[0].split('-')
-                # print(anno_mese)
                 
                 anno = int(anno_mese[0])
                 mese = int(anno_mese[1])
 
-                # print(anno)
-                # print(mese)
 
                 #siamo alla prima iterazione
                 if count == 0:
@@ -100,12 +126,8 @@ class CSVTimeSeriesFile:
                 
                 count+=1
 
-                # print('Max anno: {}'.format(anno_max))
-                # print('Max mese: {}'.format(mese_max))
                 
-
-                
-                #lista da append a data, comprende solo la data e il numero ignorando la presenza di                    altri elementi nella riga, se trova i primi due, l'iterazione continua
+                #lista da append a data, comprende solo la data e il numero ignorando la presenza di altri elementi nella riga, se trova i primi due, l'iterazione continua
                 final_elements = []
 
                 final_elements.append(elements[0])
@@ -133,7 +155,9 @@ def compute_increments(time_series, first_year, last_year):
 
     #verifico che i parametri first_year e last_year siano stringhe altrimenti alzo un'eccezione
     if not isinstance(first_year, str) or not isinstance(last_year, str):
-        raise ExamException("Errore: Uno o più parametri passati in input come intervallo, non è una stringa.")
+        raise ExamException("Errore : Uno o più parametri passati in input come intervallo, non è una stringa.")
+
+
 
     
     # Creazione del nuovo dizionario con le differenze tra le medie degli anni nell'intervallo
@@ -145,6 +169,17 @@ def compute_increments(time_series, first_year, last_year):
     
     #dizionario di appoggio che ha come key l'anno e come valore il numero di passeggeri. Se la misurazione non esiste per un determinato mese non importa
     anno_valori = dizionario_anno_valori(time_series)
+
+
+    if first_year not in anni_vuoti and first_year not in anno_valori.keys():
+        raise ExamException("Errore : L'anno passato come parametro iniziale non è presente nel file. Ricorda che l'anno deve essere scritto con le cifre arabe (0-9) e senza lettere o simboli")
+
+    if last_year not in anni_vuoti and last_year not in anno_valori.keys():
+        raise ExamException("Errore : L'anno passato come parametro finale non è presente nel file. Ricorda che l'anno deve essere scritto con le cifre arabe (0-9) e senza lettere o simboli")
+
+    #se uno dei due anni come parametri non ha dati e sono consecutivi
+    if (last_year not in anno_valori.keys() or first_year not in anno_valori.keys()) and (int(last_year) - int(first_year) == 1):
+        return []
     
     for anno, valori in anno_valori.items():
         
@@ -154,29 +189,52 @@ def compute_increments(time_series, first_year, last_year):
         # Aggiunta della chiave e della media dei valori al nuovo dizionario
         anno_media[anno] = media_valori_annua
 
-
+    
+    #******************************
+    #  Gestione casi speciali
+    #******************************
+    
+    #Se l'anno inziale e l'anno finale coincidono stampo returno semplicemente l'anno desiderato col suo valore di media. Tuttavia se l'anno inserito è lo stesso ma non presenta alcuna misurazione, viene returnata una lista vuota
+    if first_year == last_year:
+        if first_year in anno_media.keys():
+            return {first_year: anno_media[first_year]}
+        else:
+            return []
+    
+    #se l'anno iniziale e finale sono due anni che non presentano dati, ma sono presenti nel file, e hanno in mezzo un anno con dati presenti. ALlora stampo l'anno in mezzo. Esempio 1950 e 1952. Dove 1950 e 1952 non hanno dati, ma 1951 si, stampa quello (1951)
+    if (first_year not in anno_media.keys() and first_year in anni_vuoti) and (last_year not in anno_media.keys() and last_year in anni_vuoti) and (int(last_year) - int(first_year) == 2):
+        anno_in_mezzo = str(int(first_year) + 1)
+        return {anno_in_mezzo: anno_media[anno_in_mezzo]}
+        
+        
     #ora prendo l'intervallo di anni voluto dal dizionario
 
     #anni è una lista che contiene tutte le chiavi (anni) del dizionario che contiene anni e media annua, dove non sono presenti anni che avevano valori nulli precedentemente
     anni = list(anno_media.keys())
-        
+
     #usiamo len anni - 1 perchè avendo anni consecutivi (utilizzando il i + 1) rischiamo di andare oltre alla lunghezza della lista e avere errori se non facessimo in questo modo
     for i in range(len(anni) - 1):
         #anno iniziale e finale saranno due anni consecutivi
         anno_iniziale = anni[i]
         anno_finale = anni[i + 1]
 
+        
         # Verifica se l'anno rientra nell'intervallo desiderato
-        if first_year <= anno_iniziale < last_year:
+        if first_year <= anno_iniziale <= last_year:
             # Crea la chiave dell'intervallo di anni (es. "1950-1951")
             intervallo_anni = "{}-{}".format(anno_iniziale, anno_finale)
 
+
+            #se last year passato come parameteo non ha dati al suo interno, interrompe il ciclo. Casistica intervallo '49-'52, dati mancanti 1950 e 1952. Ideologicamente dovrebbe restituire 1949-1950 1950-1951 1951-1952. Ma dato che mancano quei due anni deve restituire 1949-1951 e basta. Questo controllo impedisce di memorizzare anche 1951-1953 che andrebbe oltre il limite alto imposto
+            if(int(anno_finale) > int(last_year)):
+                break
+            
+            
             # Calcola la differenza tra le medie degli anni consecutivi
             differenza_media = anno_media[anno_finale] - anno_media[anno_iniziale]
-            
+
             # Aggiungi la chiave e la differenza al nuovo dizionario, arrotondata ad 1 decimale
-            intervalli_valori[intervallo_anni] = round(differenza_media, 1)
-            
+            intervalli_valori[intervallo_anni] = round(differenza_media, 1)            
     return intervalli_valori
 
 
@@ -234,14 +292,10 @@ def verifica_pattern(data):
         return False
 
 
-
-
 # Esempio di utilizzo
+# time_series_file = CSVTimeSeriesFile(name='dataProva.csv')
+# time_series = time_series_file.get_data()
+# print(time_series)
 
-#testing
-time_series_file = CSVTimeSeriesFile(name='data.csv')
-time_series = time_series_file.get_data()
-print(time_series)
-
-dizionario = compute_increments(time_series, '1949', '1952')
-print(dizionario)
+# dizionario = compute_increments(time_series, '1949', '1960')
+# print(dizionario)
